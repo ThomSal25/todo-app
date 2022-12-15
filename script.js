ccfa54269d3bcf8df9e60faa7b2f030423363285
Thomas Salmann <thomas.salmann@gmail.com>
2022-12-15 11:54:55 +0100

    add: gets information about li's

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const btnElement = document.querySelector("#btn-new-todo");
 const newTodoInputElement = document.querySelector("#new-todo");
 const todoListElement = document.querySelector("#todo-list");
+const allListElements = document.querySelectorAll("ul li")
 
 function addNewTodo() {
-  console.log("Click");
-
   // TODO Nur Todo erzeugen wenn Textfeld nicht leer
-  if (newTodoInputElement.value.length >= 2) {
+  if (newTodoInputElement.value.length >= 5) {
     // checkbox Element erzeugen
     const checkboxElement = document.createElement("input");
     checkboxElement.type = "checkbox";
@@ -20,6 +19,7 @@ function addNewTodo() {
     // li Element new todo input text zuweisen
     const textNode = document.createTextNode(newTodoInputElement.value);
     liElement.appendChild(textNode);
+    // newTodoInputElement.value = "";
 
     checkboxElement.addEventListener("change", () => {
       liElement.classList.toggle("is-done");
@@ -29,9 +29,58 @@ function addNewTodo() {
     todoListElement.appendChild(liElement);
   } else {
     alert(
-      "Sorry ich konnte das Todo nicht hinzufügen, es muss mindestens 2 Zeichen beinhalten."
+      "Sorry ich konnte das Todo nicht hinzufügen, es muss mindestens 5 Zeichen beinhalten."
     );
-  }
+    }
+    // console.log(allListElements, document.querySelectorAll("ul li"))
+    showLi()
 }
 
 btnElement.addEventListener("click", addNewTodo);
+
+// filter function
+
+const allOpen = document.querySelector("#all-todos");
+const openTodos = document.querySelector("#open-todos");
+const doneTodos = document.querySelector("#done-todos");
+
+function showListElement() {
+  if (allOpen.checked === true) {
+    // show all li --> class .is-done is not important --> remove .out-of-filter from all
+    console.log("👍");
+  } else if (openTodos.checked === true) {
+    // show open li --> has not class .is-done --> add .out-of-filter to .is-done
+    console.log("👍👍");
+  } else if (doneTodos.checked === true) {
+    // show done li --> has class .is-done --> add .out-of-filter, when not is-done
+    console.log("👍👍👍👍");
+  }
+}
+
+function checkClassList() {
+  // li.classList.contains("is-done") --> return true
+}
+
+allOpen.addEventListener("change", showListElement);
+openTodos.addEventListener("change", showListElement);
+doneTodos.addEventListener("change", showListElement);
+
+
+function showLi() { // show all list elements of the todo ul
+    for (let i = 0; i < document.querySelectorAll("ul li").length; i++) {
+        // console.log(document.querySelectorAll("ul li")[i])
+        // jedesmal wenn eine checkbox gechanged wird, soll die gesamte Liste überprüft werden, welche li's noch gecheckt sind
+        document.querySelectorAll("ul li")[i].addEventListener("change", checkIsDone)
+    }
+}
+
+function checkIsDone() {
+    for (let item of document.querySelectorAll("ul li")) {
+        if (item.classList.contains("is-done")) {
+            
+            console.log(item)
+        }
+    }
+}
+
+
